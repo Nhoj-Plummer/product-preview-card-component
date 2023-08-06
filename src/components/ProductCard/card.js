54c758af0d86f656d07059b0364ea067775f6697
Nhoj <nhojplummer@yahoo.com>
2023-08-06 13:32:23 -0400

    Progress so far

--- src/components/ProductCard/card.js
+++ src/components/ProductCard/card.js
@@ -1,11 +1,12 @@
 import React from "react"
 import "./card.css"
+import { desktopimg, mobileimg, carticon } from "./cardindex"
 
 function Card() {
   return (
     <div className="product-container">
       <div className="product-container__image">
-        
+        <img src={desktopimg} />
       </div>
       <div className="prodcut-container__text"></div>
     </div>
---
+++ src/components/ProductCard/cardindex.js
@@ -0,0 +1,5 @@
+import { default as desktopimg } from "../../assets/image-product-desktop.jpg"
+import { default as mobileimg } from "../../assets/image-product-mobile.jpg"
+import { default as carticon } from "../../assets/icon-cart.svg"
+
+export { desktopimg, mobileimg, carticon }
