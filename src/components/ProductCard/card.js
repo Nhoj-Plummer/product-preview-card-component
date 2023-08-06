30110bb4496b6201e77291663f361da9ea340a7c
Nhoj <nhojplummer@yahoo.com>
2023-08-06 17:49:39 -0400

    Adding more to challenge

--- src/components/ProductCard/card.js
+++ src/components/ProductCard/card.js
@@ -4,11 +4,22 @@ import { desktopimg, mobileimg, carticon } from "./cardindex"
 
 function Card() {
   return (
-    <div className="product-container">
+    <div className="product-container section-padding">
       <div className="product-container__image">
         <img src={desktopimg} />
       </div>
-      <div className="prodcut-container__text"></div>
+      <div className="prodcut-container__text">
+        <p>Perfume</p>
+        <h1>Gabrielle Essence Eau De Parfum</h1>
+        <p>A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL</p>
+        <h2>
+          $149.99 <span>$169.99</span>
+        </h2>
+        <button id="cart-button">
+          <img src={carticon} />
+          <h3>Add To Cart</h3>
+        </button>
+      </div>
     </div>
   )
 }
