8fdf421ae0dafef09036cedb4c6db95a4e20e289
Nhoj <nhojplummer@yahoo.com>
2023-08-06 18:15:20 -0400

    Resatrting the responsiveness to get a better format

--- src/components/ProductCard/card.js
+++ src/components/ProductCard/card.js
@@ -6,19 +6,19 @@ function Card() {
   return (
     <div className="product-container section-padding">
       <div className="product-container__image">
-        <img src={desktopimg} />
+        {/* <img src={desktopimg} alt="desktop" /> */}
       </div>
-      <div className="prodcut-container__text">
-        <p>Perfume</p>
+      <div className="product-container__text">
+        {/* <p>Perfume</p>
         <h1>Gabrielle Essence Eau De Parfum</h1>
         <p>A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL</p>
         <h2>
           $149.99 <span>$169.99</span>
         </h2>
         <button id="cart-button">
-          <img src={carticon} />
+          <img src={carticon} alt="cart" />
           <h3>Add To Cart</h3>
-        </button>
+        </button> */}
       </div>
     </div>
   )
